c566e15355c2b404fdb08fd1821fdea1ad4ac0d3
DMN-sohan <65285526+DMN-sohan@users.noreply.github.com>
2024-07-15 02:56:29 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,146 @@
+import os
+import yaml
+import random
+import model
+import numpy as np
+from glob import glob
+from easydict import EasyDict
+from PIL import Image, ImageOps
+from torch import optim
+
+import utils
+from dataset import StegaData
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import lpips
+
+with open('./cfg/setting.yaml', 'r') as f:
+    args = EasyDict(yaml.load(f, Loader=yaml.SafeLoader))
+
+if not os.path.exists(args.checkpoints_path):
+    os.makedirs(args.checkpoints_path)
+
+if not os.path.exists(args.saved_models):
+    os.makedirs(args.saved_models)
+
+def load_checkpoint(model, optimizer , optimizer_secret, filename):
+    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
+    if os.path.isfile(filename):
+        print("=> loading checkpoint '{}'".format(filename))
+        checkpoint = torch.load(filename)
+        start_step = checkpoint['step']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        optimizer_secret.load_state_dict(checkpoint['secret_optimizer'])
+        
+        print("=> loaded checkpoint '{}' (step {})"
+                  .format(filename, checkpoint['step']))
+    else:
+        print("=> no checkpoint found at '{}'".format(filename))
+
+    return model, optimizer, optimizer_secret ,start_step
+
+def main():
+    for arg in args.items():
+        print(f"{arg}")
+    
+    log_path = os.path.join(args.logs_path, str(args.exp_name))
+    writer = SummaryWriter(log_path)
+
+    dataset = StegaData(args.train_path, args.secret_size, size=(400, 400))
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+
+    encoder = model.StegaStampEncoder()
+    decoder = model.StegaStampDecoder(secret_size=args.secret_size)
+    discriminator = model.Discriminator()
+    lpips_alex = lpips.LPIPS(net="alex", verbose=False)
+    if args.cuda:
+        encoder = encoder.cuda()
+        decoder = decoder.cuda()
+        discriminator = discriminator.cuda()
+        lpips_alex.cuda()
+
+    d_vars = discriminator.parameters()
+    g_vars = [{'params': encoder.parameters()},
+              {'params': decoder.parameters()}]
+
+    optimize_loss = optim.Adam(g_vars, lr=args.lr)
+    optimize_secret_loss = optim.Adam(g_vars, lr=args.lr)
+    optimize_dis = optim.RMSprop(d_vars, lr=0.00001)
+
+    global_step = 0
+
+    if args.pretrained != 'None':
+        encoder_filepath = os.path.join(args.pretrained, 'encoder.pth')
+        encoder, optimize_loss, optimize_secret_loss, global_step = load_checkpoint(encoder, optimize_loss,optimize_secret_loss,encoder_filepath)
+        decoder_filepath = os.path.join(args.pretrained, 'decoder.pth')
+        decoder, optimize_loss, optimize_secret_loss, global_step = load_checkpoint(decoder, optimize_loss,optimize_secret_loss,decoder_filepath)
+
+    height = 400
+    width = 400
+
+    total_steps = len(dataset) // args.batch_size + 1
+    
+
+    while global_step < args.num_steps:
+        for _ in range(min(total_steps, args.num_steps - global_step)):
+            image_input, secret_input = next(iter(dataloader))
+            if args.cuda:
+                image_input = image_input.cuda()
+                secret_input = secret_input.cuda()
+            no_im_loss = global_step < args.no_im_loss_steps
+            l2_loss_scale = min(args.l2_loss_scale * global_step / args.l2_loss_ramp, args.l2_loss_scale)
+            lpips_loss_scale = min(args.lpips_loss_scale * global_step / args.lpips_loss_ramp, args.lpips_loss_scale)
+            secret_loss_scale = min(args.secret_loss_scale * global_step / args.secret_loss_ramp,
+                                    args.secret_loss_scale)
+            G_loss_scale = min(args.G_loss_scale * global_step / args.G_loss_ramp, args.G_loss_scale)
+            l2_edge_gain = 0
+            if global_step > args.l2_edge_delay:
+                l2_edge_gain = min(args.l2_edge_gain * (global_step - args.l2_edge_delay) / args.l2_edge_ramp,
+                                   args.l2_edge_gain)
+
+            rnd_tran = min(args.rnd_trans * global_step / args.rnd_trans_ramp, args.rnd_trans)
+            rnd_tran = np.random.uniform() * rnd_tran
+
+            global_step += 1
+            Ms = utils.get_rand_transform_matrix(width, np.floor(width * rnd_tran), args.batch_size)
+            if args.cuda:
+                Ms = Ms.cuda()
+
+            loss_scales = [l2_loss_scale, lpips_loss_scale, secret_loss_scale, G_loss_scale]
+            yuv_scales = [args.y_scale, args.u_scale, args.v_scale]
+            loss, secret_loss, D_loss, bit_acc, str_acc = model.build_model(encoder, decoder, discriminator, lpips_alex,
+                                                                            secret_input, image_input,
+                                                                            args.l2_edge_gain, args.borders,
+                                                                            args.secret_size, Ms, loss_scales,
+                                                                            yuv_scales, args, global_step, writer)
+            if no_im_loss:
+                optimize_secret_loss.zero_grad()
+                secret_loss.backward()
+                optimize_secret_loss.step()
+            else:
+                optimize_loss.zero_grad()
+                loss.backward()
+                optimize_loss.step()
+                if not args.no_gan:
+                    optimize_dis.zero_grad()
+                    optimize_dis.step()
+
+            if global_step % 1000 == 0:
+                encoder_state = {'step': global_step + 1, 'state_dict': encoder.state_dict(),
+                        'optimizer': optimize_loss.state_dict() , 'secret_optimizer': optimize_secret_loss.state_dict()}
+                torch.save(encoder_state, os.path.join(args.saved_models, "encoder.pth"))
+
+                decoder_state = {'step': global_step + 1, 'state_dict': decoder.state_dict(),
+                        'optimizer': optimize_loss.state_dict() , 'secret_optimizer': optimize_secret_loss.state_dict()}
+                torch.save(decoder_state, os.path.join(args.saved_models, "decoder.pth"))
+
+            if global_step % 100 == 0:
+                print(f'Iter #{global_step}: Loss = {loss:.4f}, secret loss = {secret_loss:.4f}, D_loss = {D_loss:.4f}, bit_acc = {bit_acc:.4f}, str_acc = {str_acc:.4f}', flush=True)
+    writer.close()
+    
+
+
+if __name__ == '__main__':
+    main()
